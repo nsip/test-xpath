e57c312bd32b1c118e408a800c482f251ebbabca
Nick Nicholas <opoudjis@optusnet.com.au>
2019-04-04 10:02:54 +1100

    debug

--- test1.go
+++ test1.go
@@ -4,34 +4,30 @@ package main
 //  - https://github.com/antchfx/xpath
 //    . https://github.com/antchfx/xmlquery
 
-
 // /NAPStudentResponseSet[PathTakenForDomain and not(PathTakenForDomain/@xsi:nil = ‘true’)]/TestletList/Testlet/ItemResponseList/ItemResponse/Response
 
 import (
+	"fmt"
 	"github.com/antchfx/xmlquery"
-  "os"
-  "fmt"
+	"os"
 )
 
 func main() {
-  f, err := os.Open("sif.xml")
-  doc, err := xmlquery.Parse(f)
-  if err != nil {
-    panic(err)
-  }
-  fmt.Printf("Loaded ok\n")
-  fmt.Printf("DOC\n%+v\n", doc)
+	f, err := os.Open("sif.xml")
+	doc, err := xmlquery.Parse(f)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("Loaded ok\n")
+	fmt.Printf("DOC\n%+v\n", doc)
 
-  channel := xmlquery.FindOne(doc, "//SchoolInfo")
-  fmt.Printf("CHANNEL\n%+v\n", channel)
+	channel := xmlquery.FindOne(doc, "//SchoolInfo")
+	fmt.Printf("CHANNEL\n%+v\n", channel)
 
-  // Query for Nick
-  channel2 := xmlquery.FindOne(
-    doc,
-    "/NAPStudentResponseSet[PathTakenForDomain and not(PathTakenForDomain/@xsi:nil = ‘true’)]/TestletList/Testlet/ItemResponseList/ItemResponse/Response")
-  fmt.Printf("CHANNEL2\n%+v\n", channel2)
-
-
-
+	// Query for Nick
+	channel2 := xmlquery.FindOne(
+		doc,
+		"//NAPStudentResponseSet[PathTakenForDomain and not(PathTakenForDomain/@xsi:nil = 'true')]/TestletList/Testlet/ItemResponseList/ItemResponse/Response")
+	fmt.Printf("CHANNEL2\n%+v\n", channel2.OutputXML(true))
 
 }
